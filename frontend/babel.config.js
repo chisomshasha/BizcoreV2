3c8f5b3b39435f89f730f5a776ef0841c11e4085
chisomshasha <chisomarinzeshasha@gmail.com>
2026-04-17 16:37:07 +0200

    Update babel.config.js

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,17 +1,20 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      // react-native-dotenv plugin (if you really need it)
       ['module:react-native-dotenv', {
-      'react-native-reanimated/plugin',
         moduleName: '@env',
         path: '.env',
         blacklist: null,
         whitelist: null,
         safe: false,
         allowUndefined: true
-      }]
+      }],
+
+      // This MUST be the LAST plugin
+      'react-native-reanimated/plugin'
     ]
   };
 };
